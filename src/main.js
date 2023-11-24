47c10e6ff5dc7e8a6be69e091e177e2c2571ef00
Dani094 <117084576+SandyGarciaR@users.noreply.github.com>
2023-11-24 14:44:13 -0500

    install quasar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import {Quasar} from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
+const myApp = createApp(App)
+
+myApp.use(Quasar,{
+    plugins:{},
+})
+
 
 createApp(App).mount('#app')
